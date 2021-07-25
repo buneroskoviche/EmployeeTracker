1e7b2e762431eb5a858ffed3680612a415a43366
Buney <dpkinser93@gmail.com>
2021-07-24 18:41:21 -0600

    all list functions now display in a table

--- queries/listAll.js
+++ queries/listAll.js
@@ -7,49 +7,76 @@ module.exports = {
         // Use a switch to determine the category
         switch (category) {
             case 'department':
+                // Will display detailed department information
                 try {
                     // Find all data in a category
                     const dptData = await Department.findAll();
                     // Remove extra fluff from the instances
                     const departments = dptData.map(entry => entry.get({plain: true}));
-                    // Console log the data values
-                    departments.forEach(entry => console.log(entry));
+                    // Create a table class
+                    const table = new Table({
+                        head: ['ID', 'Name']
+                    });
+                    // Insert data into the new table
+                    departments.forEach(dpt => table.push([dpt.id, dpt.name]));
+                    console.log(table.toString());
                 } catch (e) {
                     console.log(e);
                 }    
                 break;
             case 'role':
+                // Will display detailed role informaiton
                 try {
-                    const roleData = await Role.findAll();
+                    const roleData = await Role.findAll({
+                        include: [{model: Department}]
+                    });
                     const roles = roleData.map(entry => entry.get({plain: true}));
-                    roles.forEach(entry => console.log(entry));
+                    const table = new Table({
+                        head: ['ID', 'Title', 'Salary', 'Department']
+                    });
+                    roles.forEach(role => table.push([
+                        role.id, 
+                        role.title, 
+                        role.salary, 
+                        role.department.name
+                    ]));
+                    console.log(table.toString());
                 } catch (e) {
                     console.log(e);
                 } 
                 break;
             case 'employee':
+                // Will display detailed employee information
                 try {
-                    const empData = await Employee.findAll();
+                    const empData = await Employee.findAll({
+                        include: [{model: Role}]
+                    });
                     const employees = empData.map(entry => entry.get({plain: true}));
-                    employees.forEach(entry => console.log(entry));
+                    const table = new Table({
+                        head: ['ID', 'First Name', 'Last Name', 'Role', 'Manager']
+                    });
+                    employees.forEach(employee => table.push([
+                        employee.id, 
+                        employee.first_name,
+                        employee.last_name,
+                        employee.role.title,
+                        employee.manager || 'None'
+                    ]));
+                    console.log(table.toString());
                 } catch (e) {
                     console.log(e);
                 } 
                 break;
             default:
-                // Default will list all model relations
+                // Default will display all basic information
                 try {
-                    // Get all employee data and related items
                     const roughData = await Employee.findAll( {
                         include: [{model: Role, include: [{model: Department}]}]
                     });
-                    // Break down the data so it is easy to work with
                     const ezData = roughData.map(entry => entry.get({plain: true}));
-                    // Create a new table
                     const table = new Table({
-                        head: ['Name', 'Role', 'Deparment']
+                        head: ['Name', 'Role', 'Department']
                     });
-                    // Insert data into the table
                     ezData.forEach(entry => table.push([
                         `${entry.first_name} ${entry.last_name}`,
                         entry.role.title,
